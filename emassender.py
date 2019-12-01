8f55d9fe43efc6fed2a99b36e36b66021bd7cd64
evilden <evilden91@gmail.com>
2019-12-02 01:54:35 +0300

    emassender for sendgrid with attchments

--- emassender.py
+++ emassender.py
@@ -1,30 +1,43 @@
 import sys
 import smtplib
+import os
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
+from email.mime.base import MIMEBase
+from email import encoders
 
 
 class EmailMassSender:
-    def __init__(self, mail_user, mail_password, subject, text):
-        self.mail_user = mail_user
-        self.mail_password = mail_password
+    def __init__(self, sender_email, sendgrid_api_key, subject, text, attached_file_path=None):
+        self.sender_email = sender_email
+        self.sendgrid_api_key = sendgrid_api_key
         self.subject = subject
         self.text = text
+        self.attached_file_path = attached_file_path
 
     def send_to_list(self, email_list):
+        if self.attached_file_path is not None:
+            part = MIMEBase('application', "octet-stream")
+            part.set_payload(open(self.attached_file_path, "rb").read())
+            encoders.encode_base64(part)
+            part.add_header('Content-Disposition',
+                            'attachment; filename={0}'.format(os.path.basename(self.attached_file_path)))
+
         for to in email_list:
             msg = MIMEMultipart()
-            msg['From'] = self.mail_user
+            msg['From'] = self.sender_email
             msg['To'] = to
             msg['Subject'] = self.subject
             msg.attach(MIMEText(self.text, 'plain'))
+            if self.attached_file_path is not None:
+                msg.attach(part)
+
             text_msg = msg.as_string()
             try:
-                server = smtplib.SMTP_SSL('smtp.mail.ru', 465)
-                # server.set_debuglevel(1)
+                server = smtplib.SMTP_SSL('smtp.sendgrid.net', 465)
                 server.ehlo()
-                server.login(self.mail_user, self.mail_password)
-                server.sendmail(self.mail_user, to, text_msg)
+                server.login('apikey', self.sendgrid_api_key)
+                server.sendmail(self.sender_email, to, text_msg)
                 server.close()
                 print('Email sent!')
             except Exception as e:
@@ -33,13 +46,14 @@ class EmailMassSender:
 
 
 def main():
-    mail_user = 'account@mail.ru'
-    mail_password = 'password'
-    subject = 'yo dog!'
-    text = "where is my money?"
+    sender_email = 'ninja@virtualsquad.com'
+    sendgrid_api_key = 'apikey'
+    subject = 'subject'
+    text = "text of the message"
+    attached_file_path = 'AI.pdf'
 
-    to_list = ['mail1@gmail.com', 'mail2@gmail.com']
-    sender = EmailMassSender(mail_user, mail_password, subject, text)
+    to_list = ['evilden91@gmail.com', 'downtownxore@gmail.com']
+    sender = EmailMassSender(sender_email, sendgrid_api_key, subject, text, attached_file_path)
     sender.send_to_list(to_list)
 
 
