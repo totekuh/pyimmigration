42b578ae4239c8a3759e272683897a4e8fd13028
retr0 <you@example.com>
2019-12-01 19:14:49 -0500

    fix threads performance

--- email_harvester.py
+++ email_harvester.py
@@ -15,7 +15,7 @@ DATASET_DIR = 'dataset'
 HARVEST_FILE = 'harvest.txt'
 threads_limit = 50
 scraper_threads = []
-sleep_timer_in_seconds = 1
+sleep_timer_in_seconds = 10
 
 # we don't need it
 EMAIL_BLACKLIST = ['noreply@indeed.com', '@sentry.indeed.com']
@@ -30,34 +30,22 @@ def read_captures_emails(harvest_file=HARVEST_FILE):
 
 
 class EmailScraper:
-    def __init__(self, line, file=HARVEST_FILE):
-        self.company = line.split('###')[0]
-        self.url = line.split('###')[1]
+    def __init__(self, file=HARVEST_FILE):
         self.output_file = file
-        self.thread = Thread(target=self.find_email, args=())
 
-    def start(self):
-        self.thread.start()
+    def find_email(self, company, url):
 
-    def is_alive(self):
-        return self.thread.isAlive()
-
-    def find_email(self):
-        global all_jobs_count
-        global contacts
-        global running_scrapers
-        logging.info(f'Collecting emails from {self.company} '
-                     f'running: {len(running_scrapers)}; finished: {len(contacts)}; remaining: {all_jobs_count}')
         try:
-            resp = requests.get(self.url)
+            resp = requests.get(url)
             if resp.ok:
                 emails = scrape_emails(resp.text)
                 if emails:
                     for email in emails:
                         if not any(junk in email for junk in EMAIL_BLACKLIST) and email not in read_captures_emails():
                             self.save_email(email)
+
             else:
-                logging.warning(f'{self.company} has returned unexpected status code: {resp.status_code}')
+                logging.warning(f'{company} has returned unexpected status code: {resp.status_code}')
         except Exception as e:
             logging.error(e)
 
@@ -81,24 +69,14 @@ def parse_contacts_files(dataset_dir):
 contacts = parse_contacts_files(dataset_dir=DATASET_DIR)
 
 all_jobs_count = len(contacts)
-running_scrapers = []
+finished_jobs_count = 0
 
-while len(contacts) != 0:
-    if len(running_scrapers) >= threads_limit:
-        sleep(sleep_timer_in_seconds)
-    for running_scraper in running_scrapers.copy():
-        if not running_scraper.is_alive():
-            running_scrapers.remove(running_scraper)
-    for line in contacts.copy():
-        try:
-            scraper = EmailScraper(line)
-            scraper.start()
-            running_scrapers.append(scraper)
-            contacts.remove(line)
-        except Exception as e:
-            logging.error(e)
+scraper = EmailScraper()
+for line in contacts:
+    company = line.split('###')[0]
+    url = line.split('###')[1]
+    logging.info(f'Collecting emails from {company} [{finished_jobs_count}/{all_jobs_count}]')
+    scraper.find_email(company, url)
+    finished_jobs_count += 1
 
-while any(scraper.is_alive() for scraper in running_scrapers):
-    sleep(sleep_timer_in_seconds)
-
-logging.info(f'All scrapers have finished with {len(read_captures_emails())} emails')
+logging.info(f'All jobs have finished with {len(read_captures_emails())} emails')
