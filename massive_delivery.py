0d482223fcbd47270f617df4af6cbcd563ff36c3
derstolz <derstolz@kali>
2020-09-28 23:11:02 +0300

    fixed a few bugs

--- massive_delivery.py
+++ massive_delivery.py
@@ -12,16 +12,11 @@ logging.basicConfig(format='[%(asctime)s %(levelname)s]: %(message)s',
                     datefmt='%m/%d/%Y %I:%M:%S %p',
                     level='INFO')
 
-API_KEY_FILE = 'apikey.txt'
 DEFAULT_USED_EMAILS_FILE = 'used_emails.txt'
 DEFAULT_SUBJECT = 'Job Application'
 DEFAULT_ATTACH_FILENAME = 'cv.pdf'
 DEFAULT_MAIL_TEXT_FILENAME = 'text.txt'
-DEFAULT_SMTP_HOST = 'smtp.sendgrid.net'
 DEFAULT_SMTP_PORT = 465
-
-with open(API_KEY_FILE, 'r') as f:
-    sendgrid_api_key = f.read().strip()
 
 
 def get_arguments():
@@ -33,42 +28,51 @@ def get_arguments():
                         required=True,
                         help='An email address or a '
                              'txt file with a new-line separated list of email addresses to '
-                             'make a massive delivery.')
+                             'perform a massive delivery to.')
     parser.add_argument('--file',
                         dest='file',
                         default=DEFAULT_ATTACH_FILENAME,
                         required=False,
-                        help='Attach a file to your email. Default is ' + DEFAULT_ATTACH_FILENAME)
+                        help='Pass this argument to attach a file to your email. '
+                             f"Default is {DEFAULT_ATTACH_FILENAME}")
     parser.add_argument('--sender',
                         dest='sender',
                         required=True,
-                        help='A name to use as a sender name')
+                        help='A name to use as the sender name')
     parser.add_argument('--subject',
                         dest='subject',
                         default=DEFAULT_SUBJECT,
                         required=False,
-                        help='A mail subject to use. Default is ' + DEFAULT_SUBJECT)
+                        help=f"A mail subject to use. Default is {DEFAULT_SUBJECT}")
     parser.add_argument('--text',
                         dest='text',
                         default=DEFAULT_MAIL_TEXT_FILENAME,
                         required=False,
-                        help='The mail text. This argument might be used as a string or as a file name. Default is '
-                             + DEFAULT_ATTACH_FILENAME)
+                        help='The mail text. This argument might be used as a string or as a file name. '
+                             f"Default is '{DEFAULT_ATTACH_FILENAME}'")
     parser.add_argument('--used-emails',
                         dest='used_emails',
                         default=DEFAULT_USED_EMAILS_FILE,
                         required=False,
                         help='A txt file to write used emails.')
+    parser.add_argument('--smtp-login',
+                        dest='smtp_login',
+                        required=True,
+                        help='An SMTP login to use for authentication')
+    parser.add_argument('--smtp-password',
+                        dest='smtp_password',
+                        required=True,
+                        help='An SMTP password to use for authentication')
     parser.add_argument('--smtp-host',
                         dest='smtp_host',
-                        default=DEFAULT_SMTP_HOST,
-                        required=False,
-                        help='An SMTP host to use. Default is ' + DEFAULT_SMTP_HOST)
+                        required=True,
+                        help='An SMTP host to use')
     parser.add_argument('--smtp-port',
                         dest='smtp_port',
                         default=DEFAULT_SMTP_PORT,
+                        type=int,
                         required=False,
-                        help='An SMTP port to use. Default is ' + str(DEFAULT_SMTP_PORT))
+                        help=f"An SMTP port to use. Default is {DEFAULT_SMTP_PORT}")
     options = parser.parse_args()
 
     return options
@@ -77,16 +81,18 @@ def get_arguments():
 class EmailMassSender:
     def __init__(self,
                  sender_email,
-                 sendgrid_api_key,
                  subject,
-                 attached_file_path=None,
-                 smtp_host=DEFAULT_SMTP_HOST,
+                 smtp_login,
+                 smtp_password,
+                 smtp_host,
                  smtp_port=DEFAULT_SMTP_PORT,
+                 attached_file_path=None,
                  used_emails_file='used_emails.txt'):
         self.smtp_host = smtp_host
         self.smtp_port = smtp_port
+        self.smtp_login = smtp_login
+        self.smtp_password = smtp_password
         self.sender_email = sender_email
-        self.sendgrid_api_key = sendgrid_api_key
         self.subject = subject
         self.attached_file_path = attached_file_path
         self.used_emails_file = used_emails_file
@@ -109,13 +115,13 @@ class EmailMassSender:
 
         text_msg = msg.as_string()
         if recipient_address in self.read_used_emails():
-            logging.warning(f'Skipping {recipient_address} as it was used before')
+            logging.warning(f'Skipping {recipient_address} as it was already used')
         else:
             try:
-                logging.info(f'Sending email to {recipient_address}')
+                logging.info(f'Sending an email to {recipient_address}')
                 server = smtplib.SMTP_SSL(self.smtp_host, self.smtp_port)
                 server.ehlo()
-                server.login('apikey', self.sendgrid_api_key)
+                server.login(self.smtp_login, self.smtp_password)
                 server.sendmail(self.sender_email, recipient_address, text_msg)
                 server.close()
                 self.store_used_email(recipient_address)
@@ -125,7 +131,7 @@ class EmailMassSender:
     def store_used_email(self, email_address):
         with open(self.used_emails_file, 'a') as used_f:
             used_f.write(email_address)
-            used_f.write('\n')
+            used_f.write(os.linesep)
 
     def read_used_emails(self):
         if Path(self.used_emails_file).exists():
@@ -152,14 +158,15 @@ sender_email = options.sender
 subject = options.subject
 
 sender = EmailMassSender(sender_email=sender_email,
+                         smtp_login=options.smtp_login,
+                         smtp_password=options.smtp_password,
                          smtp_host=options.smtp_host,
-                         smtp_port=int(options.smtp_port),
-                         sendgrid_api_key=sendgrid_api_key,
+                         smtp_port=options.smtp_port,
                          subject=subject,
                          attached_file_path=options.file,
                          used_emails_file=options.used_emails)
 for email in emails:
     sender.send_to(email, text)
 
-# sending the last email to the sender in order to confirm the delivery
+# send an email to the sender in order to confirm the delivery
 sender.send_to(sender_email, f'Massive email delivery has finished with {len(emails)} emails.')
